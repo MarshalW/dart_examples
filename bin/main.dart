73c456a809d96e2d124dbb9cbd1660eafa8b936a
marshal <marshal.wu@qq.com>
2018-12-02 14:31:17 +0800

    finished homework

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,10 @@
+// https://flutter.io/docs/cookbook/networking/fetch-data
+// 参见上述链接文档
+
 import 'package:dart_examples/dart_examples.dart' as dart_examples;
 
-main(List<String> arguments) {
+main(List<String> arguments) async {
   print('Hello world: ${dart_examples.calculate()}!');
+  var result=await dart_examples.get();
+  print('result message: ${result.message}');
 }
--- lib/dart_examples.dart
+++ lib/dart_examples.dart
@@ -1,3 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// 一个免费的新闻restful api
+var url =
+    'https://newsapi.org/v1/articles?apikey=3e22f2fcc1344975ae2b2e69379e2a6e&sortBy=latest&source=techcrunch';
+
+Future<Result> get() async {
+  var res = await http.get(url);
+  return Result.fromJson(json.decode(res.body));
+}
+
 int calculate() {
   return 6 * 7;
+}
+
+class Result {
+  final String message;
+
+  Result({this.message});
+
+  factory Result.fromJson(Map<String, dynamic> json) {
+    return Result(message: json['message']);
+  }
 }
