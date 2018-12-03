c45653cc28614a42dd3122b83a5c8ddb77c9d401
marshal <marshal.wu@qq.com>
2018-12-03 21:06:39 +0800

    add commonents

--- bin/main.dart
+++ bin/main.dart
@@ -7,6 +7,7 @@ main(List<String> arguments) async {
   var result=await dart_examples.get();
   print('result message: ${result.message}');
   print('-----------');
+  print('github users:');
   var users=await dart_examples.getUsers();
   users.forEach((e)=>print(e.userName));
 
