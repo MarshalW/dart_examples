021044758e6849a0476e1cab13d27660bae9ea9e
marshal <marshal.wu@qq.com>
2018-12-03 21:31:13 +0800

    finished homework

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,45 @@
 import 'package:dart_examples/dart_examples.dart' as dart_examples;
 
 main(List<String> arguments) {
-  print('Hello world: ${dart_examples.calculate()}!');
+  testSingleDemo();
+  testBroadcastDemo();
+}
+
+/**
+ * test single subscription stream
+ */
+void testSingleDemo() {
+  var demo = dart_examples.SingleDemo();
+  demo.listen((message) {
+    print('single subscription receive message: $message');
+  });
+
+  // 下面语句将报异常：Bad state: Stream has already been listened to.
+  // demo.listen((message){
+  //   print('test another customer: $message');
+  // });
+
+  demo.sendMessage('hello world.');
+  demo.sendMessage('good job!');
+  demo.close();
+
+  // 下面语句将报异常：Bad state: Cannot add event after closing
+  // demo.sendMessage('another message');
+}
+
+/**
+ * test broadcast stream
+ */
+void testBroadcastDemo() {
+  var demo = dart_examples.BroadcastDemo();
+  demo.listen((message) {
+    print('broadcast customer A, receive message: $message');
+  });
+  demo.listen((message) {
+    print('broadcast customer B, receive message: $message');
+  });
+
+  demo.sendMessage('hello world.');
+  demo.sendMessage('good job!');
+  demo.close();
 }
--- lib/dart_examples.dart
+++ lib/dart_examples.dart
@@ -1,3 +1,39 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:async';
+
+/**
+ * use single subscription stream controller demo
+ */
+class SingleDemo {
+  StreamController<String> _controller = new StreamController();
+
+  void listen(callback) {
+    _controller.stream.listen(callback);
+  }
+
+  void sendMessage(message) {
+    _controller.add(message);
+  }
+
+  void close() {
+    _controller.close();
+  }
+}
+
+/**
+ * use broadcast stream controller demo
+ */
+class BroadcastDemo {
+  StreamController<String> _controller = new StreamController.broadcast();
+
+  void listen(callback) {
+    _controller.stream.listen(callback);
+  }
+
+  void sendMessage(message) {
+    _controller.add(message);
+  }
+
+  void close() {
+    _controller.close();
+  }
 }
