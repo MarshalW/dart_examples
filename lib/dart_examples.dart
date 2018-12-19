62351f02c6cd29267a61eaefd5879b42532b540e
marshal <marshal.wu@qq.com>
2018-12-19 16:07:37 +0800

    update demo

--- lib/dart_examples.dart
+++ lib/dart_examples.dart
@@ -1,10 +1,15 @@
 library my_lib;
 
+import 'desc.dart' show desc;
+import 'desc.dart' show intro;
+// import 'desc.dart' hide intro;
 part 'product.dart';
 
 class Product {
   @override
   String toString() {
+    desc();
+    intro(); // 如果import没有show，就不能用
     return 'Product';
   }
 }
